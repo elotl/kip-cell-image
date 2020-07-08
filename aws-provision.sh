4780acad3d8e06dcf28d5d5bb713e238e5ca4c09
Vilmos Nebehaj <v.nebehaj@gmail.com>
2020-07-08 08:43:03 -0700

    Fail if curl fails in provisioning script

--- aws-provision.sh
+++ aws-provision.sh
@@ -3,8 +3,8 @@
 set -euxo pipefail
 
 DIST=$(. /etc/os-release; echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/libnvidia-container/gpgkey | sudo apt-key add -
-curl -s -L https://nvidia.github.io/libnvidia-container/$DIST/libnvidia-container.list | sudo tee /etc/apt/sources.list.d/libnvidia-container.list
+curl -sfL https://nvidia.github.io/libnvidia-container/gpgkey | sudo apt-key add -
+curl -sfL https://nvidia.github.io/libnvidia-container/$DIST/libnvidia-container.list | sudo tee /etc/apt/sources.list.d/libnvidia-container.list
 sudo add-apt-repository -y ppa:graphics-drivers/ppa
 sudo apt-get update -y
 sudo apt-get install -y iproute2 ipset iptables nfs-common ssl-cert libnvidia-container-tools
