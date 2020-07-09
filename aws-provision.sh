eecdad43c331973be4ac7747d68d88f94b8aa798
Vilmos Nebehaj <v.nebehaj@gmail.com>
2020-07-09 09:03:46 -0700

    Update td-agent service file

--- aws-provision.sh
+++ aws-provision.sh
@@ -11,11 +11,14 @@ sudo apt-get install -y iproute2 ipset iptables nfs-common ssl-cert libnvidia-co
 sudo apt-get install -y --no-install-recommends nvidia-cuda-toolkit nvidia-430
 
 curl -sfL https://toolbelt.treasuredata.com/sh/install-ubuntu-xenial-td-agent3.sh | sh
+sudo sed -i '/^User=.*$/d' /lib/systemd/system/td-agent.service
+sudo sed -i '/^Group=.*$/d' /lib/systemd/system/td-agent.service
 sudo apt-get install -y g++ make
 sudo /opt/td-agent/embedded/bin/fluent-gem install fluent-plugin-cloudwatch-logs
 sudo /opt/td-agent/embedded/bin/fluent-gem install fluent-plugin-kubernetes_metadata_filter
 sudo apt-get remove -y g++ make
 sudo mv /tmp/aws-fluentd-cell.conf /etc/td-agent/td-agent.conf
+sudo systemctl daemon-reload
 sudo systemctl enable td-agent
 
 sudo dpkg -i /tmp/$KIP_PACKAGE
