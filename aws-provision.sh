b98119f9929f95a4e21a29c8e9abc02d53e29eb3
Vilmos Nebehaj <v.nebehaj@gmail.com>
2020-07-13 12:47:30 -0700

    Test fluentd with pinned plugin versions

--- aws-provision.sh
+++ aws-provision.sh
@@ -10,9 +10,27 @@ sudo apt-get update -y
 sudo apt-get install -y iproute2 ipset iptables nfs-common ssl-cert libnvidia-container-tools
 sudo apt-get install -y --no-install-recommends nvidia-cuda-toolkit nvidia-430
 
-curl -sfLO https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
-sudo dpkg -i amazon-cloudwatch-agent.deb && rm amazon-cloudwatch-agent.deb
-sudo cp /tmp/aws-cloudwatch-agent.conf /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+curl -sfL https://toolbelt.treasuredata.com/sh/install-ubuntu-xenial-td-agent3.sh | sh
+sudo sed -i '/^User=.*$/d' /lib/systemd/system/td-agent.service
+sudo sed -i '/^Group=.*$/d' /lib/systemd/system/td-agent.service
+sudo apt-get install -y g++ make
+sudo /opt/td-agent/embedded/bin/fluent-gem install --version 0.7.4 fluent-plugin-cloudwatch-logs
+sudo /opt/td-agent/embedded/bin/fluent-gem install --version 2.3.0 fluent-plugin-concat
+sudo /opt/td-agent/embedded/bin/fluent-gem install --version 2.5.1 fluent-plugin-grok-parser
+sudo /opt/td-agent/embedded/bin/fluent-gem install --version 1.0.2 fluent-plugin-json-in-json-2
+sudo /opt/td-agent/embedded/bin/fluent-gem install --version 2.3.0 fluent-plugin-kubernetes_metadata_filter
+sudo /opt/td-agent/embedded/bin/fluent-gem install --version 1.0.0 fluent-plugin-multi-format-parser
+sudo /opt/td-agent/embedded/bin/fluent-gem install --version 1.5.0 fluent-plugin-prometheus
+sudo /opt/td-agent/embedded/bin/fluent-gem install --version 2.0.1 fluent-plugin-record-modifier
+sudo /opt/td-agent/embedded/bin/fluent-gem install --version 2.2.0 fluent-plugin-rewrite-tag-filter
+sudo /opt/td-agent/embedded/bin/fluent-gem install --version 1.0.2 fluent-plugin-systemd
+sudo apt-get remove -y g++ make
+sudo mv /tmp/aws-fluentd-cell.conf /etc/td-agent/td-agent.conf
+sudo systemctl daemon-reload
+sudo systemctl enable td-agent
+#curl -sfLO https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+#sudo dpkg -i amazon-cloudwatch-agent.deb && rm amazon-cloudwatch-agent.deb
+#sudo cp /tmp/aws-cloudwatch-agent.conf /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 
 sudo dpkg -i /tmp/$KIP_PACKAGE
 
