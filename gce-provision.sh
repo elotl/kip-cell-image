83eb7bf6d22d01276127073fb9e6f85aeef71b69
hidalgopl <pbojanowski@protonmail.com>
2020-10-27 16:16:38 +0100

    * base kip-cell-image on ubuntu 18.04 * remove ami_groups to avoid Encrypted snapshots can't be shared publicly error * install podman, enable podman.socket * install td-agent for bionic * disable kubernetes-metadata-filter plugin * verify fluentd config

--- gce-provision.sh
+++ gce-provision.sh
@@ -8,6 +8,7 @@ sudo systemd disable apt-daily-upgrade.timer || true
 sudo systemd stop apt-daily-upgrade.timer || true
 
 DIST=$(. /etc/os-release; echo $ID$VERSION_ID)
+VERSION_ID=$(. /etc/os-release; echo $VERSION_ID)
 curl -s -L https://nvidia.github.io/libnvidia-container/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/libnvidia-container/$DIST/libnvidia-container.list | sudo tee /etc/apt/sources.list.d/libnvidia-container.list
 curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh
@@ -15,13 +16,17 @@ sudo bash add-logging-agent-repo.sh
 curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh
 sudo bash add-monitoring-agent-repo.sh
 sudo add-apt-repository -y ppa:graphics-drivers/ppa
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
 sudo apt-get update -y
 sudo apt-get install -y iproute2 ipset iptables nfs-common ssl-cert google-fluentd google-fluentd-catch-all-config-structured stackdriver-agent libnvidia-container-tools
-sudo apt-get install -y --no-install-recommends nvidia-cuda-toolkit nvidia-430
+sudo apt-get install -y --no-install-recommends nvidia-cuda-toolkit nvidia-driver-430 podman
+sudo systemctl enable podman.socket
 
 sudo dpkg -i /tmp/$KIP_PACKAGE
 
 sudo mv /tmp/google-fluentd-cell.conf /etc/google-fluentd/config.d/elotl-cell.conf
+sudo /opt/google-fluentd/embedded/bin/fluentd --dry-run -c /etc/google-fluentd/config.d/elotl-cell.conf
 
 echo -e '[IpForwarding]\nip_aliases = false\n' | sudo tee /etc/default/instance_configs.cfg.template
 
@@ -33,4 +38,5 @@ sudo rm -rf /root/.ssh
 sudo rm -rf /home/packer/.ssh
 sudo rm -rf /home/ubuntu/.ssh
 
+podman system info
 itzo --version
