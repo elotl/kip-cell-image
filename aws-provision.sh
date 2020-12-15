84ea6028936533dabb3a2799789150ffacbd757c
Vilmos Nebehaj <v.nebehaj@gmail.com>
2020-12-15 14:46:51 -0800

    Add podman from kubic 18.04 repo

--- aws-provision.sh
+++ aws-provision.sh
@@ -17,14 +17,22 @@ done
 
 DIST=$(. /etc/os-release; echo $ID$VERSION_ID)
 VERSION_ID=$(. /etc/os-release; echo $VERSION_ID)
+
 curl -sfL https://nvidia.github.io/libnvidia-container/gpgkey | sudo apt-key add -
 curl -sfL https://nvidia.github.io/libnvidia-container/$DIST/libnvidia-container.list | sudo tee /etc/apt/sources.list.d/libnvidia-container.list
 sudo add-apt-repository -y ppa:graphics-drivers/ppa
-#echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-#curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
+
+echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_18.04/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_18.04/Release.key | sudo apt-key add -
+
 sudo apt-get update -y
 sudo apt-get install -y iproute2 ipset iptables nfs-common ssl-cert libnvidia-container-tools
 sudo apt-get install -y --no-install-recommends nvidia-cuda-toolkit nvidia-430
+
+sudo apt-get install -y podman
+# Remove parameter not supported by systemd on 16.04.
+sudo sed -i '/StartLimitIntervalSec/d' /usr/lib/systemd/system/podman.service
+sudo systemctl daemon-reload
 
 curl -sfL https://toolbelt.treasuredata.com/sh/install-ubuntu-xenial-td-agent3.sh | sh
 sudo sed -i '/^User=.*$/d' /lib/systemd/system/td-agent.service
@@ -50,6 +58,7 @@ sudo systemctl enable td-agent
 #sudo dpkg -i amazon-cloudwatch-agent.deb && rm amazon-cloudwatch-agent.deb
 #sudo cp /tmp/aws-cloudwatch-agent.conf /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 
+sudo systemctl enable podman.socket
 
 sudo dpkg -i /tmp/$KIP_PACKAGE
 
@@ -66,5 +75,7 @@ sudo mkdir -p /var/log/journal
 sudo rm -rf /root/.ssh
 sudo rm -rf /home/packer/.ssh
 sudo rm -rf /home/ubuntu/.ssh
+
+podman system info
 
 itzo --version
--- gce-provision.sh
+++ gce-provision.sh
@@ -16,11 +16,20 @@ sudo bash add-logging-agent-repo.sh
 curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh
 sudo bash add-monitoring-agent-repo.sh
 sudo add-apt-repository -y ppa:graphics-drivers/ppa
-#echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-#curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
+
+echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_18.04/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_18.04/Release.key | sudo apt-key add -
+
 sudo apt-get update -y
 sudo apt-get install -y iproute2 ipset iptables nfs-common ssl-cert google-fluentd google-fluentd-catch-all-config-structured stackdriver-agent libnvidia-container-tools
 sudo apt-get install -y --no-install-recommends nvidia-cuda-toolkit nvidia-430
+
+sudo apt-get install -y podman
+# Remove parameter not supported by systemd on 16.04.
+sudo sed -i '/StartLimitIntervalSec/d' /usr/lib/systemd/system/podman.service
+sudo systemctl daemon-reload
+
+sudo systemctl enable podman.socket
 
 sudo dpkg -i /tmp/$KIP_PACKAGE
 
@@ -36,5 +45,7 @@ sudo mkdir -p /var/log/journal
 sudo rm -rf /root/.ssh
 sudo rm -rf /home/packer/.ssh
 sudo rm -rf /home/ubuntu/.ssh
+
+podman system info
 
 itzo --version
