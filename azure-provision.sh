35f6288ba21110dab8cb8942765e052019ad760c
hidalgopl <pbojanowski@protonmail.com>
2021-03-16 15:54:03 +0100

    add kubic:libcontainers to sources.list

--- azure-provision.sh
+++ azure-provision.sh
@@ -11,8 +11,10 @@ DIST=$(. /etc/os-release; echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/libnvidia-container/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/libnvidia-container/$DIST/libnvidia-container.list | sudo tee /etc/apt/sources.list.d/libnvidia-container.list
 sudo add-apt-repository -y ppa:graphics-drivers/ppa
+echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_18.04/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_18.04/Release.key | sudo apt-key add -
 sudo apt-get update -y
-sudo apt-get install -y iproute2 ipset iptables nfs-common ssl-cert libnvidia-container-tools snapd
+sudo apt-get install -y iproute2 ipset iptables nfs-common ssl-cert libnvidia-container-tools
 sudo apt-get install -y --no-install-recommends nvidia-cuda-toolkit nvidia-430
 sudo apt-get install -y podman
 sudo systemctl enable podman.socket
